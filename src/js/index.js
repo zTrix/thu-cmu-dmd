ede7b8d9b018af4f705352b18608a23ace8e3be5
zTrix <i@ztrix.me>
2012-09-21 02:09:13 +0800

    version merge

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,14 @@
 
 (function ($) {
+    var current_lang = function () {
+        return $('#lang_switch').text() == 'English' ? 'cn' : 'en';
+    };
+
     $('#nav_list a').click(function (e) {
         e.preventDefault();
         var t = $(this).attr('href').substr(1);
         if (t.match(/\.index$/)) {      // load sidebar index and its first content item
-            $.getJSON(t + '.json').success(function (rs) {
+            $.getJSON(current_lang() + '/' + t + '.json').success(function (rs) {
                 $('#index_bar').css('display', 'block');
                 $('#content').removeClass('span12').addClass('span10');
                 var ul = $('#index_bar ul').empty();
@@ -12,10 +16,10 @@
                     var a = $('<a/>').attr('href', '#' + e.target).text(e.name);
                     ul.append($('<li></li>').html(a));
                 });
-                ul.children()[0].click();
+                ul.children().first().children().click();
             });
         } else {                        // load content directly
-            $.ajax({url: t + '.html'}).done(function (rs) {
+            $.ajax({url: current_lang() + '/' + t + '.html'}).done(function (rs) {
                 $('#content').removeClass('span10').addClass('span12');
                 $('#content').html(rs);
                 $('#index_bar').css('display', 'none');
@@ -28,7 +32,7 @@
     $('#index_bar ul').on('click', 'a', function (e) {
         e.preventDefault();
         var t = $(this).attr('href').substr(1);
-        $.ajax({url: t + '.html'}).done(function (rs) {
+        $.ajax({url: current_lang() + '/' + t + '.html'}).done(function (rs) {
             $('#content').html(rs);
         });
         $('#index_bar li a').parent().removeClass('active');
